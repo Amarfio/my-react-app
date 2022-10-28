78b5599e471e81fa93021df4634c288976a763b8
= <=>
2022-10-28 20:21:39 +0000

    first push for the application

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const myFirstElement = <h1>Hello React!</h1>4
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
